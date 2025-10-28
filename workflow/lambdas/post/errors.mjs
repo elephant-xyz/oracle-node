d0bb52e0f57e1e5c0860ab8263b7122d4a698771
Mykyta Ovsiienko <mykyta.ovsiienko@gmail.com>
2025-10-28 15:01:34 +0200

    docs(errors): update typedefs for new properties

--- workflow/lambdas/post/errors.mjs
+++ workflow/lambdas/post/errors.mjs
@@ -18,12 +18,19 @@
  * @property {string} SK - Secondary key for the DynamoDB table. Represents the execution error. in the format of "EXECUTION#{EXECUTION_ID}"
  * @property {string} event - Input event used to start the execution.
  * @property {string} county - County identifier.
+ * @property {string} errorsS3Uri - S3 URI for the errors csv file.
+ * @property {number} count - Count of the errors in this execution.
+ */
+
+/**
+ * @typedef {('failed' | 'maybeSolved' | 'solved')} ErrorStatus
  */
 
 /**
  * @typedef {object} ExetuionError
  * @property {string} PK - Primary key for the DynamoDB table. Same as the PK of the ErrorRecord. In the format of "ERROR#{SHA256}"
  * @property {string} SK - Secondary key for the DynamoDB table. Represent link to the execution error. int the format of "EXECUTION#{EXECUTION_ID}"
+ * @property {ErrorStatus} status - Status of the error.
  * @property {string} error - Error identifier.
  * @property {string} executionID - Execution ID.
  */
